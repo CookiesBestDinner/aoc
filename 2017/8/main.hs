86c791df819b96e2d26c475c54b8fdeb80b74342
Jonatan <hello.how@are.you>
2018-12-18 21:02:32 +0100

    add 2017/8

---
+++ 2017/8/main.hs
@@ -0,0 +1,65 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import Data.Attoparsec.Text
+import Prelude hiding (compare)
+import Data.Map (Map)
+import Data.Text (Text)
+import qualified Data.Text.IO as TIO
+import qualified Data.Map as Map
+
+main :: IO ()
+main = do
+  input <- TIO.getContents
+  let operations = case parseOnly (parseOp `sepBy` endOfLine) input of
+        Right ops -> ops
+        Left  err -> error err
+      result = scanl (flip id) Map.empty operations
+      largestValue mem | Map.null mem = 0
+                       | otherwise    = maximum $ Map.elems $ mem
+      part1 = largestValue $ last result
+      part2 = maximum $ map largestValue result
+  print part1
+  print part2
+
+operation
+  :: String
+  -> Text
+  -> Int
+  -> String
+  -> Text
+  -> Int
+  -> Map String Int
+  -> Map String Int
+operation register what howMuch condreg condop condval registers =
+  if compare (Map.findWithDefault 0 condreg registers) condval
+    then Map.insert register (op current) registers
+    else registers
+ where
+  current = Map.findWithDefault 0 register registers
+  op      = case what of
+    "dec" -> (subtract howMuch)
+    "inc" -> (+ howMuch)
+    _     -> error "no such operation"
+  compare = case condop of
+    ">"  -> (>)
+    "<"  -> (<)
+    ">=" -> (>=)
+    "<=" -> (<=)
+    "==" -> (==)
+    "!=" -> (/=)
+    _    -> error "no such comparison"
+
+parseOp :: Parser (Map String Int -> Map String Int)
+parseOp =
+  operation
+    <$> many1 letter  -- register
+    <*  " "
+    <*> choice ["inc", "dec"]
+    <*  " "
+    <*> signed decimal
+    <*  " if "
+    <*> many1 letter  -- cond register
+    <*  " "
+    <*> choice ["<=", ">=", ">", "<", "!=", "=="]
+    <*  " "
+    <*> signed decimal

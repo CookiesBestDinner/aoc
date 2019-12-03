43563c02c0febd4f6d1f3f96c331aa1342a1da27
Jonatan <hello.how@are.you>
2019-12-03 06:52:40 +0100

    2019 day 3

--- 2019/app/Main.hs
+++ 2019/app/Main.hs
@@ -8,7 +8,7 @@ import           Protolude
 
 import qualified Day01
 import qualified Day02
--- import qualified Day03
+import qualified Day03
 -- import qualified Day04
 -- import qualified Day05
 -- import qualified Day06
@@ -37,7 +37,7 @@ main = do
   case args of
     ["1"] -> Day01.main
     ["2"] -> Day02.main
-    -- ["3"] -> Day03.main
+    ["3"] -> Day03.main
     -- ["4"] -> Day04.main
     -- ["5"] -> Day05.main
     -- ["6"] -> Day06.main
---
+++ 2019/src/Day03.hs
@@ -0,0 +1,72 @@
+{-# LANGUAGE NoImplicitPrelude   #-}
+{-# LANGUAGE OverloadedStrings   #-}
+
+module Day03 where
+
+import           Data.List                                ( last )
+import qualified Data.Map.Strict               as Map
+import qualified Data.Set                      as Set
+import qualified Data.Text                     as T
+import           Protolude
+import           Text.Megaparsec
+import           Text.Megaparsec.Char.Lexer
+
+main :: IO ()
+main = do
+  [a', b'] <- T.lines <$> readFile "input/day03"
+  -- [a', b'] <- T.lines <$> readFile "input/day03ex1"
+  -- [a', b'] <- T.lines <$> readFile "input/day03ex2"
+  a        <- parseLine a'
+  b        <- parseLine b'
+  let aTour        = walk (0, 0) a
+  let bTour        = walk (0, 0) b
+  let common = Set.fromList aTour `Set.intersection` Set.fromList bTour
+  common & Set.elems <&> manhattan & minimum & print
+  -- part2
+  let mkStepDistMap tour = Map.fromListWith (flip const) (zip tour [1 ..])
+      sdma = mkStepDistMap aTour
+      sdmb = mkStepDistMap bTour
+  let totalSteps wh = sdma Map.! wh + sdmb Map.! wh
+  common & Set.elems <&> totalSteps & minimum & print
+
+manhattan :: (Integer, Integer) -> Integer
+manhattan (x, y) = abs x + abs y
+
+walk :: (Integer, Integer) -> [Dir] -> [(Integer, Integer)]
+walk here (x : xs) = steps <> walk there xs
+ where
+  steps = step here x
+  there = last steps
+walk _ [] = []
+
+step :: (Integer, Integer) -> Dir -> [(Integer, Integer)]
+step (x, y) (Dup    n) = [ (x, y + i) | i <- [1 .. n] ]
+step (x, y) (Ddown  n) = [ (x, y - i) | i <- [1 .. n] ]
+step (x, y) (Dright n) = [ (x + i, y) | i <- [1 .. n] ]
+step (x, y) (Dleft  n) = [ (x - i, y) | i <- [1 .. n] ]
+
+data Dir
+  = Dup Integer
+  | Ddown Integer
+  | Dleft Integer
+  | Dright Integer
+  deriving (Eq, Show)
+
+parseLine :: Text -> IO [Dir]
+parseLine ln = case parse pDirs "" ln of
+  Left boo -> do
+    putStr $ errorBundlePretty boo
+    exitFailure
+  Right yay -> return yay
+
+type Parser = Parsec Void Text
+
+pDirs :: Parser [Dir]
+pDirs = oneDir `sepBy` ","
+ where
+  oneDir = choice
+    [ Dup <$ "U" <*> decimal
+    , Ddown <$ "D" <*> decimal
+    , Dleft <$ "L" <*> decimal
+    , Dright <$ "R" <*> decimal
+    ]

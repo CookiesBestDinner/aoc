ba1472ee92496b5c184302371d90aadca2a29ec5
Jonatan <hello.how@are.you>
2019-12-03 05:07:08 +0100

    wip day18b

--- 2017/src/Day18.hs
+++ 2017/src/Day18.hs
@@ -6,7 +6,11 @@ module Day18 where
 
 import           Control.Lens                      hiding ( op )
 import qualified Data.Map.Strict               as Map
-import           Prelude                                  ( error )
+import           Control.Arrow
+import           Prelude                                  ( error
+                                                          , last
+                                                          , init
+                                                          )
 import           Protolude
 import           Text.Megaparsec
 import           Text.Megaparsec.Char
@@ -17,7 +21,7 @@ type Register = Map.Map Char Integer
 data Val
   = Val Integer
   | Reg Char
-  deriving (Show)
+  deriving (Eq, Show)
 
 data Op
   = Snd Val
@@ -27,7 +31,10 @@ data Op
   | Mod Val Val
   | Rcv Val
   | Jgz Val Val
-  deriving (Show)
+  deriving (Eq, Show)
+
+data Status
+  = Running | Receiving Char deriving (Eq, Show)
 
 data Computer =
   Computer
@@ -35,8 +42,10 @@ data Computer =
     , _register :: Register
     , _program  :: Map.Map Integer Op
     , _output   :: [Integer]
+    , _status :: Status
+    , _sendCount :: Int
     }
-  deriving (Show)
+  deriving (Eq, Show)
 
 makeLenses ''Computer
 
@@ -89,14 +98,65 @@ doComputerStep comp = case op' of
   action (Jgz x y) = Right . if r x <= 0 then identity else pc %~ (+ (r y - 1))
   action other     = error $ "bad instruction: " <> show other
 
+doComputerStep2 :: Computer -> Either Computer Computer
+doComputerStep2 comp
+  | comp ^. status /= Running = Left comp
+  | otherwise = case op' of
+    Nothing -> Left comp
+    Just _  -> comp & action op <&> pc %~ (+ 1)
+ where
+  r       = readVal (comp ^. register)
+  op'     = view program comp Map.!? view pc comp
+  Just op = op'
+  action :: Op -> Computer -> Either Computer Computer
+  action (Snd x        ) = Right . ((output %~ (r x :)) >>> sendCount %~ (+ 1))
+  action (Set (Reg a) b) = Right . (register . at a ?~ r b)
+  action (Add (Reg a) b) = Right . (register . at a %~ map (+ r b))
+  action (Mul (Reg a) b) = Right . (register . at a %~ map (* r b))
+  action (Mod (Reg a) b) = Right . (register . at a %~ map (`mod` r b))
+  action (Rcv (Reg x)) | r (Reg x) == 0 = Right . identity
+                       | otherwise      = Left . (status .~ Receiving x)
+  action (Jgz x y) = Right . if r x <= 0 then identity else pc %~ (+ (r y - 1))
+  action other     = error $ "bad instruction: " <> show other
+
 choochoo :: Computer -> Computer
 choochoo comp = case doComputerStep comp of
   Left  boo -> boo
   Right yay -> choochoo yay
 
+choochoos :: (Computer, Computer) -> (Computer, Computer)
+choochoos c@(a, b) =
+  let a'         = choochoo a
+      b'         = choochoo b
+      (a'', b'') = communicateab a' b
+  in  if (a', b') == (a'', b'') then (a', b') else choochoos (a'', b'')
+
+
+communicateab a b
+  | null $ b ^. output = communicateba a b
+  | otherwise = communicateba (a & putMsg & putStatusRunning) (b & popMsg)
+ where
+  msg                   = last (b ^. output)
+  (Receiving recipient) = a ^. status
+  popMsg                = output %~ init
+  putMsg                = (register . at recipient) ?~ msg
+  putStatusRunning      = status .~ Running
+
+communicateba a b | null $ a ^. output = (a, b)
+                  | otherwise = (a & popMsg, b & putMsg & putStatusRunning)
+ where
+  msg                   = last (a ^. output)
+  (Receiving recipient) = b ^. status
+  popMsg                = output %~ init
+  putMsg                = (register . at recipient) ?~ msg
+  putStatusRunning      = status .~ Running
+
 main :: IO ()
 main = do
   input <- getOps
   let instructions = Map.fromList $ zip [0 ..] input
-  let begin        = Computer 0 Map.empty instructions []
+  let begin        = Computer 0 Map.empty instructions [] Running 0
   print $ choochoo begin
+  let patchP id = (register . at 'p') ?~ id
+  putText "-------------------"
+  choochoos (begin & patchP 0, begin & patchP 1) & print
--- 2019/src/Day02.hs
+++ 2019/src/Day02.hs
@@ -1,45 +1,46 @@
-{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE NoImplicitPrelude   #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 
 module Day02 where
 
-import           Protolude
-import           Prelude                                  ( read )
-import qualified Data.Text                     as T
 import qualified Data.Map.Strict               as Map
+import qualified Data.Text                     as T
+import           Prelude                                  ( read )
+import           Protolude
 
 main :: IO ()
 main = do
   input <- getContents
   let xs :: [Int] = input & T.unpack & (\s -> "[" ++ s ++ "]") & read
-  let initialMem  = Map.fromList $ zip [0 :: Int ..] xs
-  let mkComp a b = Comp 0 patched2
-       where
-        patched1 = Map.insert 1 a initialMem
-        patched2 = Map.insert 2 b patched1
+  let initialMem  = Map.fromList $ zip [0 ..] xs
+  let mkComp a b = Comp 0 (initialMem & Map.insert 1 a & Map.insert 2 b)
   run (mkComp 12 2) & print
   print
-    [ result
+    [ a * 100 + b
     | a <- [0 .. 99]
     , b <- [0 .. 99]
     , let result = mkComp a b & run
     , result == 19690720
     ]
 
-data Comp = Comp {pc :: Int, getMem :: Map.Map Int Int} deriving Show
+data Comp =
+  Comp
+    { pc  :: Int
+    , mem :: Map.Map Int Int
+    }
+  deriving (Show)
 
 run :: Comp -> Int
 run c = case step c of
   Right c' -> run c'
-  Left  c' -> c' & getMem & (Map.! 0)
+  Left  c' -> c' & mem & (Map.! 0)
 
 step :: Comp -> Either Comp Comp
-step c@(Comp p m) = res
+step c@(Comp p m) = case i of
+  1  -> Right $ Comp (p + 4) (Map.insert o (a + b) m)
+  2  -> Right $ Comp (p + 4) (Map.insert o (a * b) m)
+  99 -> Left c
  where
-  res = case i of
-    1  -> Right $ Comp (p + 4) (Map.insert o (a + b) m)
-    2  -> Right $ Comp (p + 4) (Map.insert o (a * b) m)
-    99 -> Left c
   [i, a', b', o] = [ m Map.! n | n <- [p .. p + 3] ]
   a              = m Map.! a'
   b              = m Map.! b'

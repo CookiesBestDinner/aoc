981ece233cec990c11852b83fbdf68952cafe9dc
jonatan <j6544436085@gmail.com>
2019-12-05 09:38:45 +0100

    2019 day 5 polish

--- 2019/src/Day05.hs
+++ 2019/src/Day05.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell   #-}
 
 module Day05
   ( main
@@ -8,64 +9,65 @@ where
 
 import           Common
 
+import           Control.Arrow
+import           Control.Lens
 import qualified Data.Map.Strict               as Map
-import           Prelude                                  ( head
-                                                          , tail
-                                                          )
-import           Protolude                         hiding ( head )
+import           Protolude
 import           Text.Megaparsec
 import           Text.Megaparsec.Char.Lexer
 
-pInput :: Parser [Int]
-pInput = (signed (pure ()) decimal) `sepBy1` ","
-
-main :: Text -> IO ()
-main indata = do
-  xs <- executeParser pInput indata
-  let initialMem = Map.fromList $ zip [0 ..] xs
-  run (Comp 0 (initialMem) [1] []) & _output & print
-  run (Comp 0 (initialMem) [5] []) & _output & print
-
 data Comp =
   Comp
-    { pc      :: Int
-    , mem     :: Map.Map Int Int
+    { _pc     :: Int
+    , _mem    :: Map.Map Int Int
     , _input  :: [Int]
     , _output :: [Int]
     }
   deriving (Show)
 
+makeLenses ''Comp
+
+pInput :: Parser [Int]
+pInput = signed (pure ()) decimal `sepBy1` ","
+
+main :: Text -> IO ()
+main indata = do
+  xs <- executeParser pInput indata
+  let initialMem = Map.fromList $ zip [0 ..] xs
+  run (Comp 0 initialMem [1] []) & _output & print
+  run (Comp 0 initialMem [5] []) & _output & print
+
 run :: Comp -> Comp
 run c = case step c of
-  Right c' -> run c'
-  Left  c' -> c'
+  Just c' -> run c'
+  Nothing -> c
 
 decodeOp :: Int -> (Int, Int, Int, Int)
-decodeOp i = (10 * tens + ones, huns, thou, tenk)
- where
-  ones = i `mod` 10
-  tens = (i `div` 10) `mod` 10
-  huns = (i `div` 100) `mod` 10
-  thou = (i `div` 1000) `mod` 10
-  tenk = (i `div` 10000) `mod` 10
+decodeOp i =
+  ( i `mod` 100
+  , (i `div` 100) `mod` 10
+  , (i `div` 1000) `mod` 10
+  , (i `div` 10000) `mod` 10
+  )
 
-step :: Comp -> Either Comp Comp
-step x@(Comp p m input output) = case i of
-  1  -> Right $ Comp (p + 4) (Map.insert c (ra + rb) m) input output
-  2  -> Right $ Comp (p + 4) (Map.insert c (ra * rb) m) input output
-  3  -> Right $ Comp (p + 2) (Map.insert a (head input) m) (tail input) output
-  4  -> Right $ Comp (p + 2) m input (ra : output)
-  5  -> Right $ Comp (if ra /= 0 then rb else p + 3) m input output
-  6  -> Right $ Comp (if ra == 0 then rb else p + 3) m input output
-  7  -> Right $ Comp (p + 4) (Map.insert c (fromEnum $ ra < rb) m) input output
-  8  -> Right $ Comp (p + 4) (Map.insert c (fromEnum $ ra == rb) m) input output
-  99 -> Left x
-  _  -> panic $ "bad instruction: " <> show i
+step :: Comp -> Maybe Comp
+step comp = (if i == 99 then Nothing else Just comp) <&> case i of
+  1 -> pc %~ (+ 4) >>> mem . at c ?~ ra + rb
+  2 -> pc %~ (+ 4) >>> mem . at c ?~ ra * rb
+  7 -> pc %~ (+ 4) >>> mem . at c ?~ fromEnum (ra < rb)
+  8 -> pc %~ (+ 4) >>> mem . at c ?~ fromEnum (ra == rb)
+  3 -> pc %~ (+ 2) >>> mem . at a ?~ comp ^?! input . ix 0 >>> input %~ drop 1
+  4 -> pc %~ (+ 2) >>> output %~ (ra :)
+  5 -> pc .~ if ra /= 0 then rb else p + 3
+  6 -> pc .~ if ra == 0 then rb else p + 3
+  _ -> panic $ "bad instruction: " <> show i
  where
-  (i, ma, mb, mc) = decodeOp (m Map.! p)
-  a               = m Map.! (p + 1)
-  b               = m Map.! (p + 2)
-  c               = m Map.! (p + 3)
-  ra  = if ma == 0 then m Map.! a else a
-  rb  = if mb == 0 then m Map.! b else b
-  _rc = if mc == 0 then m Map.! c else c
+  lookup loc = comp ^?! (mem . ix loc)
+  (i, ma, mb, mc) = decodeOp (lookup p)
+  p               = comp ^. pc
+  a               = lookup (p + 1)
+  b               = lookup (p + 2)
+  c               = lookup (p + 3)
+  ra              = if ma == 0 then lookup a else a
+  rb              = if mb == 0 then lookup b else b
+  _rc             = if mc == 0 then lookup c else c

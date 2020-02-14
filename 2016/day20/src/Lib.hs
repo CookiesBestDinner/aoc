b46ff196513a1a70e17130fe36ee75076c513d97
jonatan <j6544436085@gmail.com>
2020-02-14 05:47:18 +0100

    2016 d20

---
+++ 2016/day20/src/Lib.hs
@@ -0,0 +1,36 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+module Lib where
+import           Data.List                                ( delete )
+import           Protolude
+import           Text.Megaparsec
+import           Text.Megaparsec.Char
+import           Text.Megaparsec.Char.Lexer               ( decimal )
+
+data Range = Range { begin :: Int, end :: Int } deriving (Show, Eq)
+
+inRange :: Int -> Range -> Bool
+inRange x (Range lo hi) = x >= lo && x <= hi
+
+pRanges :: Parsec Void Text [Range]
+pRanges = pRange `sepEndBy1` space <* eof
+
+pRange :: Parsec Void Text Range
+pRange = Range <$> decimal <* "-" <*> decimal
+
+day20main :: IO ()
+day20main = do
+  input  <- readFile "input"
+  parsed <- case parse pRanges "" input of
+    Right wooo -> pure wooo
+    Left  boo  -> putStr (errorBundlePretty boo) >> exitFailure
+  print $ take 1 $ findIPs parsed 0
+  print $ length $ findIPs parsed 0
+
+findIPs :: [Range] -> Int -> [Int]
+findIPs ranges ip
+  | ip > 4294967295 = []
+  | otherwise = case find (inRange ip) ranges of
+    Just r@(Range _ hi) -> findIPs (delete r ranges) (hi + 1)
+    Nothing             -> ip : findIPs ranges (ip + 1)
